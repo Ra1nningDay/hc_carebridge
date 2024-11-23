8e5bdcd45321948c173227e3b802db7cca3f6d58
Ra1nningDay <123659453+Ra1nningDay@users.noreply.github.com>
2024-11-23 19:12:24 +0700

    ลืมเพิ่ม route

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ require __DIR__.'/chatbot.php';
 require __DIR__.'/carefield.php';
 require __DIR__.'/rating.php';
 require __DIR__.'/survey.php';
+require __DIR__.'/chat.php';
 
 // ภาษา
 Route::get('lang/{locale}', function ($locale) {
